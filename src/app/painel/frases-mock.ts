24d07adb8d24db6cc58a5c5d1dd0249966380182
Andre Leonardo Correa Barbosa <andre.barbosa@avantibrpremium.local>
2019-04-10 14:31:40 -0300

    shared e mock

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from "src/shared/Frase.model";
+
+export const FRASES: Frase[] = [
+    { fraseEng: 'I like to learn', frasePtBt: 'Eu gosto de aprender' },
+    { fraseEng: 'I watch tv', frasePtBt: 'Eu assisto tv' },
+    { fraseEng: 'How are you?', frasePtBt: 'Como vai você?' },
+    { fraseEng: 'i eat bread', frasePtBt: 'Eu como pão?' },
+];
---
+++ src/shared/Frase.model.ts
@@ -0,0 +1,10 @@
+export class Frase
+{
+    public fraseEng: string;
+    public frasePtBt: string;
+
+    constructor(fraseEng:string, frasePtBr: string) {
+        this.fraseEng  = fraseEng;
+        this.frasePtBt = frasePtBr;
+    }
+}
